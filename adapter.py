314940ead83f9b1bff388967fc528ee7db8d44a0
Dhruv Kool Rajamani <dhruvkoolrajamani@gmail.com>
2019-06-03 23:51:16 -0500

    Updated config files

--- adapter.py
+++ adapter.py
@@ -8,18 +8,19 @@ __copyright__ = "Copyright 2019, The VDAQ Project"
 __date__ = "17 May 2019"
 
 import obd
+import numpy as np
 
 obd.logger.setLevel(obd.logging.DEBUG)
 
 ports = obd.scan_serial()  # return list of valid USB or RF ports
 
-if ports.len() == 0:
-    is_bluetooth_paired = False
+if len(ports) == 0:
+	is_bluetooth_paired = False
 else:
-    is_bluetooth_paired = True
+	is_bluetooth_paired = True
 
 if is_bluetooth_paired:
-    print("{} Port found, connecting to OBD Port ...".format(ports[0]))
-    connection = obd.OBD(portstr=ports[0], baudrate=9600)
+	print("{} Port found, connecting to OBD Port ...".format(ports[0]))
+	connection = obd.OBD(portstr=ports[0], baudrate=9600)
 else:
-    print("No port available on the Bluetooth Port")
+	print("No port available on the Bluetooth Port")
